621cd7205279998a9c5921b20edd185ef86516ca
pode.fr <pode.fr@gmail.com>
2011-11-26 15:38:50 +0100

    added namespace

--- src/Olives/Olives.js
+++ src/Olives/Olives.js
@@ -1,15 +1,20 @@
 function Olives(FWK) {
+	
+	var _namespace = "Olives",
+		_addNS = function (name) {
+			return _namespace + "." + name;
+		};
 		
 	this.declare = function declare(name, constructor) {
 			if (typeof name == "string" && typeof constructor == "function") {
-				return FWK.declare(name, constructor);
+				return FWK.declare(_addNS(name), constructor);
 			} else {
 				return false;
 			}
 		};
 		
 	this.require = function require(name) {
-		return FWK.require(name);
+		return FWK.require(_addNS(name));
 	};
 };
 
