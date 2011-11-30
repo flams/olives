ec7e7d546ec1b9e2137384723a97b8be1b1aeb75
pode.fr <pode.fr@gmail.com>
2011-11-30 01:11:25 +0100

    added initial view-model binding, better than the previous one though there's still work to do around memleaks!

--- src/Olives-test/_base-test.js
+++ src/Olives-test/_base-test.js
@@ -12,3 +12,46 @@ TestCase("_baseTest", {
 		assertTrue(tools.compareObjects(tinyStore, model));
 	}
 });
+
+TestCase("_baseTemplateTest", {
+	
+	setUp: function () {
+		this._base = Olives.create("_base");
+	},
+	
+	"test _base has a template property": function () {
+		assertString(this._base.template);
+	},
+	
+	"test _base has a place method to add the UI to the dom": function () {
+		var div = document.createElement("div"),
+			uiTemplate = '<p id="tag">hello</p>';
+		
+		assertFunction(this._base.place);
+		this._base.template = uiTemplate;
+		assertEquals(this._base, this._base.place(div));
+		assertEquals(1, div.querySelectorAll("p#tag").length);
+	}
+	
+});
+
+TestCase("_baseTemplateBinding", {
+
+	setUp: function () {
+		this._base = Olives.create("_base");
+		this.div = document.createElement("div");
+		this._base.template = '<p data-bind="greetingz">hello</p>';
+		this._base.place(this.div);
+	},
+	
+	"test _base template can bind view to model": function () {
+		assertTrue(this._base.model.has("greetingz"));
+		assertEquals("hello", this._base.model.get("greetingz"));
+	},
+	
+	"test _base template view is updated on model change": function () {
+		this._base.model.set("greetingz", "Olives is cool");
+		assertSame("Olives is cool", this.div.querySelector("p").innerHTML);
+	}
+	
+});
--- src/Olives/_base.js
+++ src/Olives/_base.js
@@ -12,5 +12,32 @@ function _base(API) {
 	 */
 	this.model = API.require("TinyStore").create();
 
+	/**
+	 * The UI's template.
+	 * @type string
+	 */
+	this.template = "";
 	
+	/**
+	 * Render the UI at a given place (dom node)
+	 * @param {HTMLElement} node the node where to append the UI
+	 * @returns {Object} the UI
+	 */
+	this.place = function place(node) {
+		
+		node.innerHTML = this.template;
+		// Watch out for memleaks
+		// Get this out in a function when other values will have to be bound to model
+		// Make the default innerHTML attribution overridable 
+		// still Loadss of stuff to do but it's a cool start
+		Array.prototype.forEach.call(node.querySelectorAll("*[data-bind]"), function (tag) {
+			this.model.set(tag.getAttribute("data-bind"), tag.innerHTML);
+			this.model.watch(tag.getAttribute("data-bind"), function (value) {
+				tag.innerHTML = value;
+			});
+		}, this);
+		
+		
+		return this;
+	};
 });
