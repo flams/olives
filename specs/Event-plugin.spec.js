e623cecf006e126e0abe2c1e489362bc51c0a13d
Olivier Wietrich <olivier.wietrich@gmail.com>
2012-02-12 15:59:20 +0100

    Commited by olivier, asstertion 100% event plugin

--- specs/Event-plugin.spec.js
+++ specs/Event-plugin.spec.js
@@ -1,6 +1,6 @@
-require(["Olives/Event-plugin", "Store", "Olives/OObject", "Olives/Plugins"], function (EventPlugin, Store, OObject, Plugins) {
+require(["Olives/Event-plugin"], function (EventPlugin) {
 	
-	describe("EventPluginTest", function () {
+	describe("EventPluginInit", function () {
 		
 		it("should be a constructor function", function () {
 			expect(EventPlugin).toBeInstanceOf(Function);
@@ -11,6 +11,33 @@ require(["Olives/Event-plugin", "Store", "Olives/OObject", "Olives/Plugins"], fu
 			expect(eventPlugin.listen).toBeInstanceOf(Function);
 		});
 		
+	});
+	
+	describe("EventPluginListen", function() {
+		
+		var eventPlugin = null,
+			dom = null,
+			obj = null;
+			
+		beforeEach(function() {
+			var _func = function() {
+				this.listener = function() {
+				};
+			};
+			ob = new _func();
+			dom = document.createElement("button");
+			eventPlugin = new EventPlugin(obj);
+		});
+		
+		it("Link addEventListener with the dom"), function() {
+			spyOn(dom,"addEventListener");
+			eventPlugin.listen(dom,"click","listener","true");
+			expect(dom.addEventListener.wasCalled).toEqual(true);
+			expect(dom.addEventListener.mostRecentCall.args[0]).toEqual("click");
+			expect(dom.addEventListener.mostRecentCall.args[1]).toEqual(obj["listener"]);
+			expect(dom.addEventListener.mostRecentCall.args[2]).toEqual(true);
+			
+		});
 	});
 	
 });
--- src/Event-plugin.js
+++ src/Event-plugin.js
@@ -2,10 +2,10 @@ define("Olives/Event-plugin", function () {
 	
 	return function EventPluginConstructor(parent) {
 
-		this.listen = function(node, event, listener) {
+		this.listen = function(node, event, listener, useCapture) {
 			node.addEventListener(event,function (e) {
 				parent[listener].call(parent, e, node);
-			}, true);
+			}, useCapture);
 		};	
 	};
 	
