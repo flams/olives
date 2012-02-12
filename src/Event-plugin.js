4dfbaaacdacb9df2f57584ff45e59e7d57089118
Olivier Wietrich <olivier.wietrich@gmail.com>
2012-02-12 20:18:34 +0100

    Update Event Plugin

--- src/Event-plugin.js
+++ src/Event-plugin.js
@@ -3,9 +3,9 @@ define("Olives/Event-plugin", function () {
 	return function EventPluginConstructor(parent) {
 
 		this.listen = function(node, event, listener, useCapture) {
-			node.addEventListener(event,function (e) {
-				parent[listener].call(parent, e, node);
-			}, useCapture);
+			node.addEventListener(event, function(e) { 
+				parent["listener"].call(parent,e, node);
+			}, (useCapture == "true"));
 		};	
 	};
 	
