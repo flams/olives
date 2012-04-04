4083952e0bc9d2d40c7400cd2a25f72869e745b6
Olivier Scherrer <pode.fr@gmail.com>
2012-04-04 22:10:06 +0200

    new official web page

---
+++ src/UI-plugin.js
@@ -0,0 +1,48 @@
+/**
+ * Olives
+ * Copyright(c) 2012 Olivier Scherrer <pode.fr@gmail.com> - Olivier Wietrich <olivier.wietrich@gmail.com>
+ * MIT Licensed
+ */
+
+define("Olives/UI-Plugin", 
+		
+["Olives/OObject", "Tools"],
+		
+function (OObject, Tools) {
+	
+	return function UIPluginConstructor($uis) {
+	
+		var _uis = {};
+		
+		this.place = function place(node, name) {
+			if (_uis[name] instanceof OObject) {
+				_uis[name].place(node);
+			} else {
+				throw new Error(name + " is not an OObject UI in place:"+name);
+			}
+		};
+		
+		this.set = function set(name, ui) {
+			if (typeof name == "string" && ui instanceof OObject) {
+				_uis[name] = ui;
+				return true;
+			} else {
+				return false;
+			}
+		};
+		
+		this.setAll = function setAll(uis) {
+			Tools.loop(uis, function (ui, name) {
+				this.set(name, ui);
+			}, this);
+		};
+		
+		this.get = function get(name) {
+			return _uis[name];
+		};
+		
+		this.setAll($uis);
+		
+	};
+	
+});
