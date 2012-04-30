72cb58b8053557623f2f6ef215fe8c2b4e95eb4d
Olivier Scherrer <pode.fr@gmail.com>
2012-04-30 22:24:25 +0200

    updated homepage

--- update_ghpages.sh
+++ update_ghpages.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+die () {
+    echo >&2 "$@"
+    exit 1
+}
+
+ME=`basename $0`
+
+[ "$#" -eq 1 ] || die "you should provide a version number like: ./${ME} 1.1.2"
 
 VERSION=$1
 
@@ -14,6 +22,10 @@ git checkout master tests.html
 
 #update libs
 git checkout master lib
+
+#update version in homepage
+sed -i .bak 's#version">.*<#version">'${VERSION}'<#g' index.html
+rm index.html.bak
 
 #update build files
 git checkout master build
