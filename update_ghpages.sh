2211fdf165f45fe8dd723f754bfc8a42f8f9a900
Olivier Scherrer <pode.fr@gmail.com>
2012-07-02 01:50:35 +0200

    added version to license+archive

--- update_ghpages.sh
+++ update_ghpages.sh
@@ -10,27 +10,36 @@ ME=`basename $0`
 
 VERSION=$1
 
-#update generated documentation
+echo "updating generated documentation..."
 git checkout master docs
 
-#update specs files
+echo "updating specs files for unit tests..."
 git checkout master specs
 
-#update tests
+echo "updating test runner..."
 git checkout master tools/Jasmine
 git checkout master tests.html
 
-#update libs
+echo "updating libs..."
 git checkout master lib
 
-#update version in homepage
+echo "updating the homepage with the version number..."
 sed -i .bak 's#version">.*<#version">'${VERSION}'<#g' index.html
+sed -i .bak 's#<a href="Olives.*\.tgz">#<a href="Olives-'${VERSION}'.tgz">#' index.html
 rm index.html.bak
 
-#update build files
+echo "updating the built files..."
 git checkout master build
 
-#generate Olives archive
+echo "updating license..."
+sed -i .bak 's#${VERSION}#'${VERSION}'#' build/Olives.js
+rm build/Olives.js.bak
+
+echo "removing old archive..."
+rm Olives-*.tgz
+
+echo "generating new Olives archive..."
 cp -rf build Olives-${VERSION}
-tar czf Olives.tgz Olives-${VERSION}
+tar czf Olives-${VERSION}.tgz Olives-${VERSION}
 rm -rf Olives-${VERSION}
+git add Olives-${VERSION}.tgz
