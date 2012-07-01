f489cd064b565e21cd3f61394fe238ec27a5c200
Olivier Scherrer <pode.fr@gmail.com>
2012-07-02 01:46:27 +0200

    added version to the license

--- build/Olives.js
+++ build/Olives.js
@@ -1,5 +1,5 @@
 /*
- Olives http://flams.github.com/olives
+ Olives ${VERSION} http://flams.github.com/olives
  The MIT License (MIT)
  Copyright (c) 2012 Olivier Scherrer <pode.fr@gmail.com> - Olivier Wietrich <olivier.wietrich@gmail.com>
 */
