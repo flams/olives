6d7ad9c869ad32dba85e9bf006a5db68e69caaff
Olivier Scherrer <pode.fr@gmail.com>
2013-02-06 21:25:02 +0000

    updated copyright/license

--- specs/specHelper.js
+++ specs/specHelper.js
@@ -1,7 +1,7 @@
 /**
  * Olives http://flams.github.com/olives
  * The MIT License (MIT)
- * Copyright (c) 2012 Olivier Scherrer <pode.fr@gmail.com> - Olivier Wietrich <olivier.wietrich@gmail.com>
+ * Copyright (c) 2012-2013 Olivier Scherrer <pode.fr@gmail.com> - Olivier Wietrich <olivier.wietrich@gmail.com>
  */
 
 beforeEach(function () {
