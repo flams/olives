142ea9921cc07cab29c065fce24c79e47849e3ec
Olivier Scherrer <pode.fr@gmail.com>
2013-03-14 19:45:14 +0000

    adding integration tests

---
+++ specs/integration.spec.js
@@ -0,0 +1,18 @@
+/**
+ * Olives http://flams.github.com/olives
+ * The MIT License (MIT)
+ * Copyright (c) 2012-2013 Olivier Scherrer <pode.fr@gmail.com> - Olivier Wietrich <olivier.wietrich@gmail.com>
+ */
+
+require(["OObject"],
+
+function(OObject) {
+
+
+	describe("OObject is a container for DOM elements", function () {
+
+
+
+	});
+
+});
