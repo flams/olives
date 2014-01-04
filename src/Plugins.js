bff991229db9d2a63080244b9176fff798e76c3e
Olivier Scherrer <pode.fr@gmail.com>
2014-01-04 00:28:49 -0500

    removing requirejs

--- src/Plugins.js
+++ src/Plugins.js
@@ -3,8 +3,10 @@
  * The MIT License (MIT)
  * Copyright (c) 2012-2013 Olivier Scherrer <pode.fr@gmail.com> - Olivier Wietrich <olivier.wietrich@gmail.com>
  */
+"use strict";
 
-define(["Tools", "DomUtils"],
+var Tools = require("emily").Tools,
+    DomUtils = require("./DomUtils");
 
 /**
  * @class
@@ -15,143 +17,138 @@ define(["Tools", "DomUtils"],
  * @see Model-Plugin for instance
  * @requires Tools
  */
-function Plugins(Tools, DomUtils) {
+module.exports = function PluginsConstructor($plugins) {
 
-	"use strict";
+    /**
+     * The list of plugins
+     * @private
+     */
+    var _plugins = {},
 
-	return function PluginsConstructor($plugins) {
+    /**
+     * Just a "functionalification" of trim
+     * for code readability
+     * @private
+     */
+    trim = function trim(string) {
+        return string.trim();
+    },
 
-		/**
-		 * The list of plugins
-		 * @private
-		 */
-		var _plugins = {},
+    /**
+     * Call the plugins methods, passing them the dom node
+     * A phrase can be :
+     * <tag data-plugin='method: param, param; method:param...'/>
+     * the function has to call every method of the plugin
+     * passing it the node, and the given params
+     * @private
+     */
+    applyPlugin = function applyPlugin(node, phrase, plugin) {
+        // Split the methods
+        phrase.split(";")
+        .forEach(function (couple) {
+            // Split the result between method and params
+            var split = couple.split(":"),
+            // Trim the name
+            method = split[0].trim(),
+            // And the params, if any
+            params = split[1] ? split[1].split(",").map(trim) : [];
 
-		/**
-		 * Just a "functionalification" of trim
-		 * for code readability
-		 * @private
-		 */
-		trim = function trim(string) {
-			return string.trim();
-		},
+            // The first param must be the dom node
+            params.unshift(node);
 
-		/**
-		 * Call the plugins methods, passing them the dom node
-		 * A phrase can be :
-		 * <tag data-plugin='method: param, param; method:param...'/>
-		 * the function has to call every method of the plugin
-		 * passing it the node, and the given params
-		 * @private
-		 */
-		applyPlugin = function applyPlugin(node, phrase, plugin) {
-			// Split the methods
-			phrase.split(";")
-			.forEach(function (couple) {
-				// Split the result between method and params
-				var split = couple.split(":"),
-				// Trim the name
-				method = split[0].trim(),
-				// And the params, if any
-				params = split[1] ? split[1].split(",").map(trim) : [];
+            if (_plugins[plugin] && _plugins[plugin][method]) {
+                // Call the method with the following params for instance :
+                // [node, "param1", "param2" .. ]
+                _plugins[plugin][method].apply(_plugins[plugin], params);
+            }
 
-				// The first param must be the dom node
-				params.unshift(node);
+        });
+    };
 
-				if (_plugins[plugin] && _plugins[plugin][method]) {
-					// Call the method with the following params for instance :
-					// [node, "param1", "param2" .. ]
-					_plugins[plugin][method].apply(_plugins[plugin], params);
-				}
+    /**
+     * Add a plugin
+     *
+     * Note that once added, the function adds a "plugins" property to the plugin.
+     * It's an object that holds a name property, with the registered name of the plugin
+     * and an apply function, to use on new nodes that the plugin would generate
+     *
+     * @param {String} name the name of the data that the plugin should look for
+     * @param {Object} plugin the plugin that has the functions to execute
+     * @returns true if plugin successfully added.
+     */
+    this.add = function add(name, plugin) {
+        var that = this,
+            propertyName = "plugins";
 
-			});
-		};
+        if (typeof name == "string" && typeof plugin == "object" && plugin) {
+            _plugins[name] = plugin;
 
-		/**
-		 * Add a plugin
-		 *
-		 * Note that once added, the function adds a "plugins" property to the plugin.
-		 * It's an object that holds a name property, with the registered name of the plugin
-		 * and an apply function, to use on new nodes that the plugin would generate
-		 *
-		 * @param {String} name the name of the data that the plugin should look for
-		 * @param {Object} plugin the plugin that has the functions to execute
-		 * @returns true if plugin successfully added.
-		 */
-		this.add = function add(name, plugin) {
-			var that = this,
-				propertyName = "plugins";
+            plugin[propertyName] = {
+                    name: name,
+                    apply: function apply() {
+                        return that.apply.apply(that, arguments);
+                    }
+            };
+            return true;
+        } else {
+            return false;
+        }
+    };
 
-			if (typeof name == "string" && typeof plugin == "object" && plugin) {
-				_plugins[name] = plugin;
+    /**
+     * Add multiple plugins at once
+     * @param {Object} list key is the plugin name and value is the plugin
+     * @returns true if correct param
+     */
+    this.addAll = function addAll(list) {
+        return Tools.loop(list, function (plugin, name) {
+            this.add(name, plugin);
+        }, this);
+    };
 
-				plugin[propertyName] = {
-						name: name,
-						apply: function apply() {
-							return that.apply.apply(that, arguments);
-						}
-				};
-				return true;
-			} else {
-				return false;
-			}
-		};
+    /**
+     * Get a previously added plugin
+     * @param {String} name the name of the plugin
+     * @returns {Object} the plugin
+     */
+    this.get = function get(name) {
+        return _plugins[name];
+    };
 
-		/**
-		 * Add multiple plugins at once
-		 * @param {Object} list key is the plugin name and value is the plugin
-		 * @returns true if correct param
-		 */
-		this.addAll = function addAll(list) {
-			return Tools.loop(list, function (plugin, name) {
-				this.add(name, plugin);
-			}, this);
-		};
+    /**
+     * Delete a plugin from the list
+     * @param {String} name the name of the plugin
+     * @returns {Boolean} true if success
+     */
+    this.del = function del(name) {
+        return delete _plugins[name];
+    };
 
-		/**
-		 * Get a previously added plugin
-		 * @param {String} name the name of the plugin
-		 * @returns {Object} the plugin
-		 */
-		this.get = function get(name) {
-			return _plugins[name];
-		};
+    /**
+     * Apply the plugins to a NodeList
+     * @param {HTMLElement|SVGElement} dom the dom nodes on which to apply the plugins
+     * @returns {Boolean} true if the param is a dom node
+     */
+    this.apply = function apply(dom) {
 
-		/**
-		 * Delete a plugin from the list
-		 * @param {String} name the name of the plugin
-		 * @returns {Boolean} true if success
-		 */
-		this.del = function del(name) {
-			return delete _plugins[name];
-		};
+        var nodes;
 
-		/**
-		 * Apply the plugins to a NodeList
-		 * @param {HTMLElement|SVGElement} dom the dom nodes on which to apply the plugins
-		 * @returns {Boolean} true if the param is a dom node
-		 */
-		this.apply = function apply(dom) {
+        if (DomUtils.isAcceptedType(dom)) {
 
-			var nodes;
+            nodes = DomUtils.getNodes(dom);
+            Tools.loop(Tools.toArray(nodes), function (node) {
+                Tools.loop(DomUtils.getDataset(node), function (phrase, plugin) {
+                    applyPlugin(node, phrase, plugin);
+                });
+            });
 
-			if (DomUtils.isAcceptedType(dom)) {
+            return dom;
 
-				nodes = DomUtils.getNodes(dom);
-				Tools.loop(Tools.toArray(nodes), function (node) {
-					Tools.loop(DomUtils.getDataset(node), function (phrase, plugin) {
-						applyPlugin(node, phrase, plugin);
-					});
-				});
+        } else {
+            return false;
+        }
+    };
 
-				return dom;
+    this.addAll($plugins);
 
-			} else {
-				return false;
-			}
-		};
-
-		this.addAll($plugins);
-
-	};
-});
+};
---
+++ src/olives.js
@@ -0,0 +1,12 @@
+module.exports = {
+	"Bind.plugin": require("./Bind.plugin"),
+	"DomUtils": require("./DomUtils"),
+	"Event.plugin": require("./Event.plugin"),
+	"LocalStore": require("./LocalStore"),
+	"LocationRouter": require("./LocationRouter"),
+	"OObject": require("./OObject"),
+	"Place.plugin": require("./Place.plugin"),
+	"Plugins": require("./Plugins"),
+	"SocketIOTransport": require("./SocketIOTransport"),
+	"Stack": require("./Stack")
+};
