631fe24060566543a9e7374b4d1a70366219f07a
Olivier Scherrer <pode.fr@gmail.com>
2014-04-29 23:27:10 -0400

    remove unused

--- olives-server.js
+++
@@ -1,6 +0,0 @@
-/**
- * @license Olives http://flams.github.com/olives
- * The MIT License (MIT)
- * Copyright (c) 2012-2013 Olivier Scherrer <pode.fr@gmail.com> - Olivier Wietrich <olivier.wietrich@gmail.com>
- */
-module.exports = require("socketio-transport").Server;
